a609f8031f747175959cbff9e574b9f00c31eb90
Aarkane <31449662+Aarkane@users.noreply.github.com>
2018-02-15 10:48:03 -0800

    Created Four dimensional tic tac toe
    made variable positions and win conditions

---
+++ 4d3t.py
@@ -0,0 +1,111 @@
+import random
+
+def FourD3t()
+    def positions()
+        aw1='_'
+        ax1='_'
+        ay1='_'
+        az1='_'
+        bw1='_'
+        bx1='_'
+        by1='_'
+        bz1='_'
+        cw1='_'
+        cx1='_'
+        cy1='_'
+        cz1='_'
+        dw1='_'
+        dx1='_'
+        dy1='_'
+        dz1='_'
+        aw2='_'
+        ax2='_'
+        ay2='_'
+        az2='_'
+        bw2='_'
+        bx2='_'
+        by2='_'
+        bz2='_'
+        cw2='_'
+        cx2='_'
+        cy2='_'
+        cz2='_'
+        dw2='_'
+        dx2='_'
+        dy2='_'
+        dz2='_'
+        aw3='_'
+        ax3='_'
+        ay3='_'
+        az3='_'
+        bw3='_'
+        bx3='_'
+        by3='_'
+        bz3='_'
+        cw3='_'
+        cx3='_'
+        cy3='_'
+        cz3='_'
+        dw3='_'
+        dx3='_'
+        dy3='_'
+        dz3='_'
+        aw4='_'
+        ax4='_'
+        ay4='_'
+        az4='_'
+        bw4='_'
+        bx4='_'
+        by4='_'
+        bz4='_'
+        cw4='_'
+        cx4='_'
+        cy4='_'
+        cz4='_'
+        dw4='_'
+        dx4='_'
+        dy4='_'
+        dz4='_'
+    def play()
+        winner = 'none'
+        while winner = 'none'
+            if aw1='x' and ax1='x' and ay1='x' and az1='x':
+                winner = 'x'
+            elif aw2='x' and ax2='x' and ay2='x' and az2='x':
+                winner = 'x'
+            elif aw3='x' and ax3='x' and ay3='x' and az3='x':
+                winner = 'x'
+            elif aw4='x' and ax4='x' and ay4='x' and az4='x':
+                winner = 'x'
+            elif bw1='x' and bx1='x' and by1='x' and bz1='x':
+                winner = 'x'
+            elif bw2='x' and bx2='x' and by2='x' and bz2='x':
+                winner = 'x'
+            elif bw3='x' and bx3='x' and by3='x' and bz3='x':
+                winner = 'x'
+            elif bw4='x' and bx4='x' and by4='x' and bz4='x':
+                winner = 'x'
+            elif cw1='x' and cx1='x' and cy1='x' and cz1='x':
+                winner = 'x'
+            elif cw2='x' and cx2='x' and cy2='x' and cz2='x':
+                winner = 'x'
+            elif cw3='x' and cx3='x' and cy3='x' and cz3='x':
+                winner = 'x'
+            elif cw4='x' and cx4='x' and cy4='x' and cz4='x':
+                winner = 'x'
+            elif dw1='x' and dx1='x' and dy1='x' and dz1='x':
+                winner = 'x'
+            elif dw2='x' and dx2='x' and dy2='x' and dz2='x':
+                winner = 'x'
+            elif dw3='x' and dx3='x' and dy3='x' and dz3='x':
+                winner = 'x'
+            elif dw4='x' and dx4='x' and dy4='x' and dz4='x':
+                winner = 'x'
+            elif aw1='x' and bw1='x' and cw1='x' and dw1='x':
+                winner = 'x'
+            elif aw2='x' and bw2='x' and cw2='x' and dw2='x':
+                winner = 'x'
+            elif aw3='x' and bw3='x' and cw3='x' and dw3='x':
+                winner = 'x'
+            elif aw4='x' and bw4='x' and cw4='x' and dw4='x':
+                winner = 'x'
