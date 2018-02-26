099808929e7db8949f219bdbbfc0d7752c5cada2
Aarkane <31449662+Aarkane@users.noreply.github.com>
2018-02-26 10:27:03 -0800

    Add files via upload

---
+++ MazeMaps.py
@@ -0,0 +1,271 @@
+def maze1():
+    print "                                O--||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze2():
+    print "                                -O-||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze3():
+    print "                                --O||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze4():
+    print "                                ---||||||||||||||"
+    print "                                ||O||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze5():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||O||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze6():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||O||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze7():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||O---||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze8():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||-O--||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze9():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||--O-||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze10():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||---O||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze11():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||O||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze12():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||O||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze13():
+    print "                                ---||||||||||||||"
+    print "                                ||X||O---||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze14():
+    print "                                ---||||||||||||||"
+    print "                                ||X||-O--||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze15():
+    print "                                ---||||||||||||||"
+    print "                                ||X||--O-||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze16():
+    print "                                ---||||||||||||||"
+    print "                                ||X||---O||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze17():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||O||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze18():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||O||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze19():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||O---||X||"
+    print "                                ||||||||||||||---"
+    
+def maze20():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||-O--||X||"
+    print "                                ||||||||||||||---"
+    
+def maze21():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||--O-||X||"
+    print "                                ||||||||||||||---"
+    
+def maze22():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||---O||X||"
+    print "                                ||||||||||||||---"
+    
+def maze23():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||O||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze24():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||O||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze25():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||O---||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze26():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||-O--||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze27():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||--O-||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze28():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||---O||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze29():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||O||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze30():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||O||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||---"
+    
+def maze31():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||O||"
+    print "                                ||||||||||||||---"
+    
+def maze32():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||O--"
+    
+def maze33():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||-O-"
+    
+def maze34():
+    print "                                ---||||||||||||||"
+    print "                                ||X||----||----||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||X||X||X||X||X||"
+    print "                                ||----||----||X||"
+    print "                                ||||||||||||||--O"
